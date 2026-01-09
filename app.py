cd0f8f2059fa4386d787cc35d3b20a1f3266a4e2
Happy Kumari <137107490+Happy0936@users.noreply.github.com>
2026-01-10 00:47:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -223,13 +223,15 @@ def predict_page():
 
 
         # ---- Similarity check ----
-        preds = model.predict(img_array)
-        confidence = float(np.max(preds))
-        predicted_class = class_names[np.argmax(preds)]
+        img_feat = image.img_to_array(img)
+        img_feat = np.expand_dims(img_feat, axis=0)
+        img_feat = preprocess_input(img_feat)
+        img_embedding = feature_model.predict(img_feat)
+        similarities = cosine_similarity(img_embedding, train_features)
+        max_similarity = np.max(similarities)
 
 
-        CONFIDENCE_THRESHOLD = 0.60
-        if max_similarity < CONFIDENCE_THRESHOLD:
+        if max_similarity < 0.6:
             predicted_class = "Not a valid crop image"
             description = "The uploaded image does not match any trained crop classes."
             symptoms = []
